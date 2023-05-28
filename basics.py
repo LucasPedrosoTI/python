ad85ccaa083159a5ebc0769f4991823e122527b9
LucasPedrosoTI <lps1010@gmail.com>
2023-05-28 11:12:21 -0300

    Types and functions

--- basics.py
+++ basics.py
@@ -1,6 +1,30 @@
+"""
+This file contains the basics of Python
+Suchs as language syntax, basic features, libraries, and functions
+"""
 # To print something, use print command
 print("Python version is great")
 
 # To get input from user, use input command
-name: str = input("Type your name: ")
-print(f"Welcome to Python, {name}")
+# name: str = input("Type your name: ")
+# print(f"Welcome to Python, {name}")
+
+# Types
+age: int = 29
+height: float = 1.75
+weight = 80
+is_overwight = False
+
+# functions
+def calc_bmi(weight: float, height: float) -> float:
+  return weight/(height**2)
+
+bmi = calc_bmi(weight, height)
+is_overwight = bmi > 29.9
+print(f"BMI calculation: {bmi}")
+print(f"Overwight? {'yes' if is_overwight else 'no'}")
+
+# Getting the type
+print(type(bmi))
+# Verifying the type
+print(isinstance(bmi, float))
