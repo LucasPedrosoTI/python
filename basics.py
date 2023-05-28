1956676be5b630076d6f4f9d3a18cc1c4c5a4df4
LucasPedrosoTI <lps1010@gmail.com>
2023-05-28 15:07:20 -0300

    array methods and dicts

--- basics.py
+++ basics.py
@@ -1,3 +1,4 @@
+import copy
 import random
 
 """
@@ -9,6 +10,7 @@ print("Python version is great")
 
 # To get input from user, use input command
 # name: str = input("Type your name: ")
+name = 'Lucas'
 # print(f"Welcome to Python, {name}")
 
 # Types
@@ -78,8 +80,53 @@ for value in arr:
 for i in range(5, 10):
   print(i * 10)
 
+# Lists
 # defines start, end, and interval values 
 arr = [];
 for i in range(0, 100, 10):
   arr.append(i)
 print(arr) # [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+print(arr[0:3]) # [0, 10, 20]
+print(arr[0:6:2]) # [0, 20, 40] skips two
+
+arr.insert(2, 5) # adds item to specific index
+arr.extend([1, 2, 3]) # concats a list
+arr.pop() # removes item from the end of the list (can specify an index)
+arr.remove(1) # removes the specified item (only the first item)
+arr.count(2) # counts the number of items that matches the value
+arr.index(2) # returns the index of the first item matching the value
+arr.sort() # sorts the list, can receive boolean value 'reverse' 
+
+sum(arr) # sums the items in the list0
+max(arr) # returns the maximum number in the list
+min(arr) # returns the minimum number in the list
+
+# Dictonaries
+person = {
+  'name': name,
+  'age': age,
+  'car' : {
+    'brand': 'hyundai',
+    'model': 'hb20'
+  }
+}
+print(person)
+print(type(person))
+print(person['name'])
+
+person2 = person
+person2['name'] = 'Joe' 
+print(person, person2) # points to the same object in memory {'name': 'Joe', 'age': 41} {'name': 'Joe', 'age': 41}
+print(name) # original variable remains the same
+
+person3 = person.copy() ## shallow copy
+person3['name'] = 'Bob'
+person3['car']['model'] = 'i30' # changes the model in both
+print(person, person3) # {'name': 'Joe', 'age': 41, 'car': {'brand': 'hyundai', 'model': 'i30'}} {'name': 'Bob', 'age': 41, 'car': {'brand': 'hyundai', 'model': 'i30'}}
+
+person4 = copy.deepcopy(person)
+person4['car']['brand'] = 'honda'
+print(person, person4) # {'name': 'Joe', 'age': 41, 'car': {'brand': 'hyundai', 'model': 'i30'}} {'name': 'Joe', 'age': 41, 'car': {'brand': 'honda', 'model': 'i30'}}
+
+for key in person: # iterates over keys
+  print(person[key])
